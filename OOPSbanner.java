dc4ed4e354c1d99925c11131a9fcd225552f1367
SpI3dt <siddhab27@gmail.com>
2026-02-23 08:32:32 +0530

    Coded to Print OOPS to console

---
+++ OOPSbanner.java
@@ -0,0 +1,16 @@
+class OOPSbanner{
+	public static void main(String[] args) {
+		System.out.println("   ***   " + " " + "   ***   " + " " + "******   " + " " + "   ***** ");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "**    ** " + " " + " **      ");
+		System.out.println("**     **" + " " + "**     **" + " " + "**     **" + " " + "**       ");
+		System.out.println("**     **" + " " + "**     **" + " " + "**    ** " + " " + " **      ");
+		System.out.println("**     **" + " " + "**     **" + " " + "******   " + " " + "   ***   ");
+		System.out.println("**     **" + " " + "**     **" + " " + "**       " + " " + "      ** ");
+		System.out.println("**     **" + " " + "**     **" + " " + "**       " + " " + "       **");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "**       " + " " + "      ** ");
+		System.out.println("   ***   " + " " + "   ***   " + " " + "**       " + " " + " *****   ");
+		
+		
+		
+	}
+}
