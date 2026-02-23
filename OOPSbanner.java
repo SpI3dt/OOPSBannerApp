8e83a5147ee168d3a00f2fedd89e7471c39733de
SpI3dt <siddhab27@gmail.com>
2026-02-23 09:30:45 +0530

    Update OOPSbanner.java

--- OOPSbanner.java
+++ OOPSbanner.java
@@ -1,3 +1,13 @@
+/**
+  *uc1-print banner using print statements
+  *Author Siddharth B
+  *version @v1.0
+  */
+
+
+
+
+
 class OOPSbanner{
 	public static void main(String[] args) {
 		System.out.println("   ***   " + " " + "   ***   " + " " + "******   " + " " + "   ***** ");
