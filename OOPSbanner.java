b21adb27f5e74f00382674dc44c25eaa974449b9
SpI3dt <siddhab27@gmail.com>
2026-02-23 09:31:37 +0530

    Update OOPSbanner.java

--- OOPSbanner.java
+++ OOPSbanner.java
@@ -1,3 +1,12 @@
+/**
+  *UC2 printing oops banner 
+  *Author Siddharth B
+  *Version @ V2.0
+ */
+
+
+
+
 class OOPSbanner{
 	public static void main(String[] args) {
 		System.out.println("   ***   " + " " + "   ***   " + " " + "******   " + " " + "   ***** ");
