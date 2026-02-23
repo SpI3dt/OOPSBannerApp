346228f9f020699a8ccdee5168ef5f1dd0be5172
SpI3dt <siddhab27@gmail.com>
2026-02-23 09:04:34 +0530

    Coded UC3 to render OOPS as banner using String.join() statements

---
+++ OOPSBannerApp.java.java
@@ -0,0 +1,19 @@
+/**
+ *OOPSBannerAPP UC3- OOPS Banner application ( Use case 3 ) 	
+ *uses String.join() for better approach
+
+
+
+public class OOPSBannerApp{
+	public static void main(String[] args){
+		System.out.println(String.join("   ***   ","   ***   ","******   ","   ***** "));
+		System.out.println(String.join(" **   ** "," **   ** ","**    ** "," **      "));
+		System.out.println(String.join("**     **","**     **","**     **","**       "));
+		System.out.println(String.join("**     **","**     **","**    ** "," **      ");
+		System.out.println(String.join("**     **","**     **","******   ","   ***   "));
+		System.out.println(String.join("**     **","**     **","**       ","      ** "));
+		System.out.println(String.join("**     **","**     **","**       ","       **"));
+		System.out.println(String.join(" **   ** "," **   ** ","**       ","      ** "));
+		System.out.println(String.join("   ***   ","   ***   ","**       "," *****   "));
+	}
+}
